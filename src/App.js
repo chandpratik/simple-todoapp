092c3d25262a0e3bf1787a6221ab2e4bd98a6a96
pratikal1 <chandpratikal1@gmail.com>
2021-04-18 21:51:43 +0530

    fix empty values

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { db } from './firebase_config';
 import firebase from 'firebase';
 import TodoListItem from './Todo';
 import './styles.css';
+import { Message } from 'semantic-ui-react'
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState('');
+  const [showError, setShowError] = useState(false);
+
 
   useEffect(() => {
     getTodos();
@@ -28,15 +31,21 @@ function App() {
 
   function addTodo(e) {
     e.preventDefault();
-
-    db.collection('todos').add({
-      inprogress: true,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      todo: todoInput,
-    });
+    if (todoInput === '') {
+      setShowError(true);
+    } else {
+      db.collection('todos').add({
+        inprogress: true,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        todo: todoInput,
+      });
+      setShowError(false);
+    }
 
     setTodoInput('');
   }
+
+
 
   return (
     <>
@@ -50,26 +59,34 @@ function App() {
             width: '100%',
           }}
         >
-          <h1>TO DO-LIST ☑️</h1>
-          <form>
+          <h1 style={{ marginTop: '24px' }}>TO DO-LIST</h1>
+          {showError &&
+            <Message negative>
+              <Message.Header>We're sorry we can't add empty values</Message.Header>
+
+            </Message>}
+          <form style={{ width: '90vw', maxWidth: '600px' }}>
+
             <TextField
               id="standard-basic"
               label="Write a Todo"
               value={todoInput}
-              style={{ width: '90vw', maxWidth: '500px' }}
+              className='textField'
               onChange={(e) => setTodoInput(e.target.value)}
+              required
             />
             <Button
               type="submit"
               variant="contained"
               onClick={addTodo}
-              style={{}}
+              className='button'
+              style={{ marginTop: '10px' }}
             >
               Add ➕
             </Button>
           </form>
 
-          <div style={{ width: '90vw', maxWidth: '500px', marginTop: '24px' }}>
+          <div style={{ width: '90vw', maxWidth: '600px', marginTop: '24px' }}>
             {todos.map((todo) => (
               <TodoListItem
                 todo={todo.todo}
